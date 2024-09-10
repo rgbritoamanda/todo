9a2b17d98409f33147c3bd06a97efc9b85f076ab
rgbritoamanda <amandaramires.brito@gmail.com>
2024-09-10 16:37:49 -0300

    fundamentos 001

---
+++ index.js
@@ -0,0 +1,15 @@
+// olá, mundo!
+console.log("olá, mundo!")
+
+
+// variáveis - let
+let mensagens = "olá, mundo!"
+
+
+// variáveis - const
+const mensagem = "olá, eu"
+{
+    const mensagem = "olá, Mayk!"
+    console.log(mensagem)
+}
+console.log(mensagem);
