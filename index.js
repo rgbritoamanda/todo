9a9a9bdeff2e1fa42ff322e02f53712d30ed3871
rgbritoamanda <amandaramires.brito@gmail.com>
2024-09-10 16:42:19 -0300

    fundamentos 002

--- index.js
+++ index.js
@@ -13,3 +13,29 @@ const mensagem = "olá, eu"
     console.log(mensagem)
 }
 console.log(mensagem);
+
+// arrays
+let metas = ['mayk', 'alô']
+let metas01 = [2, 'mayk']
+// concatenando valores
+console.log(metas[1] + ", " + metas[0])
+
+let metas02 = [
+    metas,
+    {
+        value: 'caminhar 20 minutos todos os dias',
+        checked: false
+    }
+]
+console.log(metas02[1].value);
+
+// objetos
+let meta = {
+    value: 'ler um livro por mês',
+    address: 2,
+    checked: true,
+    isChecked: () => {
+        console.log(info)
+    }
+}
+console.log(meta.value);
